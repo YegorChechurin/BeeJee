d8c99d401b2eb1f63cb6fb1b3d13e4ece2dcafec
Yegor Chechurin <yegor.chechurin@gmail.com>
2019-12-08 15:04:02 +0300

    added constraints to task creation form

--- src/Service/FormBuilder.php
+++ src/Service/FormBuilder.php
@@ -5,15 +5,26 @@ namespace App\Service;
 use Symfony\Component\Form\FormFactoryInterface;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\Validator\Constraints\Email;
 
 class FormBuilder
 {
 	public static function buildTaskCreationForm(FormFactoryInterface $formFactory)
 	{
 		return $formFactory->createBuilder()
-		           ->add('username', TextType::class)
-		           ->add('email', TextType::class)
-		           ->add('text', TextType::class)
+		           ->add('username', TextType::class, [
+		           	   'constraints' => new NotBlank(),
+		           ])
+		           ->add('email', TextType::class, [
+		           	   'constraints' => [
+		           	   	   new NotBlank(),
+		           	   	   new Email(),
+		           	   ]
+		           ])
+		           ->add('text', TextType::class, [
+		           	   'constraints' => new NotBlank(),
+		           ])
 		           ->add('save', SubmitType::class, ['label' => 'Create Task'])
 		           ->getForm();
 	}
