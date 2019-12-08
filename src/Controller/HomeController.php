84f6c201d484f6d6f164acd7e7e6ff2e5243215d
Yegor Chechurin <yegor.chechurin@gmail.com>
2019-12-09 00:00:48 +0300

    finalized frontend

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -6,31 +6,27 @@ use App\Controller\AbstractController;
 use App\Database\Database;
 use App\Repository\TaskRepository;
 use App\Service\FormBuilder;
+use App\Service\TaskPaginator;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 
 class HomeController extends AbstractController
 {
-	private const TASKS_PER_PAGE = 3;
-
 	public function loadWebPage()
 	{
 		$DB = new Database();
 		$repo = new TaskRepository($DB);
-
-		$totAmounOfTasks = $repo->getTotalAmountOfTasks();
-		$amountOfPages = ceil($totAmounOfTasks/self::TASKS_PER_PAGE);
+		$paginator = new TaskPaginator($repo);
 
 		$pageNumber = $this->request->query->get('page');
-		if ($pageNumber) {
-			$offset = ($pageNumber - 1) * self::TASKS_PER_PAGE;
-		} else {
-			$offset = 0;
+		if (!$pageNumber) {
+			$pageNumber = 1;
 		}
 
-		$tasks = $repo->fetchTasksByLimit($offset, self::TASKS_PER_PAGE);
+		$tasks = $paginator->getTasksForPage((int)$pageNumber);
+
+		$paginationLimits = $paginator->getPaginationLimits((int)$pageNumber);
 
 		$form = FormBuilder::buildTaskCreationForm($this->formFactory);
-
 		$form->handleRequest($this->request);
 
 		if ($form->isSubmitted() && $form->isValid()) {
@@ -44,6 +40,10 @@ class HomeController extends AbstractController
 			[
 				'form' => $form->createView(),
 				'tasks' => $tasks,
+				'page_number' => $pageNumber,
+				'left_limit' => $paginationLimits['left_limit'],
+				'right_limit' => $paginationLimits['right_limit'],
+				'url' => strtok($this->request->getUri(), '?'),
 			]
 		);
 	}
